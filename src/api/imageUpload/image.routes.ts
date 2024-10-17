94c8e09221e53a72fa9a1ff41463b339a0941a74
love1ace <lovelacedud@gmail.com>
2024-10-17 19:08:26 +0900

    feat: AWS 이미지 여러개 업로드 가능 (#7)

--- src/api/imageUpload/image.routes.ts
+++ src/api/imageUpload/image.routes.ts
@@ -1,22 +1,22 @@
 import { Request, Response, Router } from 'express';
 import multer, { memoryStorage } from 'multer';
-import { uploadImage } from './imageUpload';
+import { uploadImages } from './imageUpload';
 
 const imageRouter = Router();
 const upload = multer({ storage: memoryStorage() });
 
 imageRouter.post(
   '/upload',
-  upload.single('image'),
+  upload.array('images', 10), // 최대 10개의 이미지를 허용합니다. 필요에 따라 조정하세요.
   async (req: Request, res: Response): Promise<void> => {
     try {
-      if (!req.file) {
+      if (!req.files || req.files.length === 0) {
         res.status(400).json({ message: '이미지 파일이 없습니다.' });
         return;
       }
 
-      const imageUrl = await uploadImage(req.file);
-      res.status(200).json({ imageUrl });
+      const imageUrls = await uploadImages(req.files as Express.Multer.File[]);
+      res.status(200).json({ imageUrls });
     } catch (error) {
       console.error('이미지 업로드 오류:', error);
       res.status(500).json({ message: '이미지 업로드 실패' });
--- src/api/imageUpload/imageUpload.ts
+++ src/api/imageUpload/imageUpload.ts
@@ -24,5 +24,29 @@ export const uploadImage = async (file: Express.Multer.File): Promise<string> =>
     throw new Error('이미지 업로드 실패');
   }
 };
+
+export const uploadImages = async (files: Express.Multer.File[]): Promise<string[]> => {
+  const uploadPromises = files.map(async (file) => {
+    const fileExtension = file.originalname.split('.').pop();
+    const fileName = `${uuidv4()}.${fileExtension}`;
+
+    const params = {
+      Bucket: process.env.AWS_BUCKET_NAME as string,
+      Key: fileName,
+      Body: file.buffer,
+      ContentType: file.mimetype,
+    };
+
+    try {
+      await s3Client.send(new PutObjectCommand(params));
+      return `${process.env.CLOUDFRONT_URL}/${fileName}`;
+    } catch (error) {
+      console.error('S3 업로드 오류:', error);
+      throw new Error('이미지 업로드 실패');
+    }
+  });
+
+  return Promise.all(uploadPromises);
+};
 // curl -X POST -H "Content-Type: multipart/form-data" -F "image=@server/src/1.jpeg" http://3.37.101.147:3000/api/images/upload
 // curl -X POST -H "Content-Type: multipart/form-data" -F "image=@src/1.jpeg" http://3.37.101.147:3000/api/images/upload
