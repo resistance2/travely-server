6e5ccd90bd148a61cdefc13932efdb3e59cacc94
love1ace <lovelacedud@gmail.com>
2024-10-17 19:22:47 +0900

    feat: 이미지 업로드 구현, 이미지 갯수 10개 제한 (#7)

--- src/api/imageUpload/image.routes.ts
+++ src/api/imageUpload/image.routes.ts
@@ -7,7 +7,7 @@ const upload = multer({ storage: memoryStorage() });
 
 imageRouter.post(
   '/upload',
-  upload.array('images', 10), // 최대 10개의 이미지를 허용합니다. 필요에 따라 조정하세요.
+  upload.array('images', 10),
   async (req: Request, res: Response): Promise<void> => {
     try {
       if (!req.files || req.files.length === 0) {
@@ -25,3 +25,7 @@ imageRouter.post(
 );
 
 export { imageRouter };
+
+// curl -X POST -H "Content-Type: multipart/form-data" -F "images=@1.jpeg" -F "images=@1.jpeg" -F "images=@1.jpeg" http://localhost:3000/api/images/upload
+// 이렇게 여러개 이미지를 넣으면 이렇게 반환됩니다.
+// {"imageUrls":["https://d25zqr3uop6qu8.cloudfront.net/fb5f5d18-61c2-4ede-b90c-4d454526b717.jpeg","https://d25zqr3uop6qu8.cloudfront.net/2dc24822-1e96-424f-b5b5-c93ea98889d3.jpeg","https://d25zqr3uop6qu8.cloudfront.net/b4282321-eb7b-4dcc-82f9-28a26da6f113.jpeg"]}%
