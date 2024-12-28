a9cc570d3c4c4f1a97505ac93d29c974e2b212e4
Ko Nak-yean <59277499+nakyeonko3@users.noreply.github.com>
2024-12-29 03:55:01 +0900

    fix: src/checkRequiredFields 수정
    - params, query를 못 받는 오류 수정

--- src/checkRequiredFields.ts
+++ src/checkRequiredFields.ts
@@ -16,7 +16,7 @@ export const checkRequiredFields = (fields: string[]) => {
 export const checkRequiredFieldsQuery = (fields: string[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
     for (const field of fields) {
-      if (!(field in req.body)) {
+      if (!(field in req.query)) {
         res.status(400).json(ResponseDTO.fail(`${field} is required`));
         return;
       }
@@ -28,7 +28,7 @@ export const checkRequiredFieldsQuery = (fields: string[]) => {
 export const checkRequiredFieldsParams = (fields: string[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
     for (const field of fields) {
-      if (!(field in req.body)) {
+      if (!(field in req.params)) {
         res.status(400).json(ResponseDTO.fail(`${field} is required`));
         return;
       }
