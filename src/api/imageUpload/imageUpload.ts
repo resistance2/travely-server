7798d59bf4916dad4684beac2a2b49869bc60cbb
Ko Nak-yean <59277499+nakyeonko3@users.noreply.github.com>
2025-03-20 22:39:46 +0900

    fix: fix: imageUpload.ts type error

--- src/api/imageUpload/imageUpload.ts
+++ src/api/imageUpload/imageUpload.ts
@@ -6,8 +6,8 @@ import { v4 as uuidv4 } from 'uuid';
 const s3Client = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
   },
 });
 
